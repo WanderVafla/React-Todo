9991019143741edccbb5ad92faf7801b16951fec
WanderVafla <procenkovladik1@gmail.com>
2026-06-03 13:35:35 +0200

    refactor: move sorting logic into TodoList component and remove utils file

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,21 +1,20 @@
 import { use, useEffect, useState, type ChangeEvent } from 'react';
 import { TodoItem } from './TodoItem';
-import { sortTaks, tasksPromise } from '../../utils';
 import { useTasks, useTasksDispatch } from '../../hooks/useTasks';
 import { FiltersNames, OrderName, TaskActionTypes } from '../../constants';
 import type { SortOption } from '../../types';
 import { useFilter } from '../../hooks/useFilter';
+import { getTasks } from '../../api';
 
+const tasksPromise = getTasks();
 const sortsOptions: SortOption[] = Object.values(OrderName);
 
 export function TodoList() {
   const tasksFromAPI = use(tasksPromise);
   const tasksDispatch = useTasksDispatch();
 
-  const [sortState, setSortState] = useState<SortOption>(
-    Object.values(OrderName)[0],
-  );
-  
+  const [sortState, setSortState] = useState<SortOption>(sortsOptions[0]);
+
   const tasks = sortTaks(useTasks(), sortState);
   const [filterState, setFilterState] = useFilter(FiltersNames);
 
@@ -69,4 +68,23 @@ export function TodoList() {
       )}
     </>
   );
+}
+
+function sortTaks(tasks, sortOption: SortOption) {
+  tasks.sort((a, b) => {
+    if (sortOption === OrderName.newest) {
+      return b.id - a.id;
+    }
+    if (sortOption === OrderName.time) {
+      if (!a.due_date && !b.due_date) return 0;
+      if (!a.due_date) return 1;
+      if (!b.due_date) return -1;
+      return new Date(a.due_date).getTime() - new Date(b.due_date).getTime();
+    }
+    if (sortOption === OrderName.name) {
+      return a.title.localeCompare(b.title);
+    }
+  });
+
+  return tasks;
 }
