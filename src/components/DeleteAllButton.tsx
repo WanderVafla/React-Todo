f74d8c9ca066a74c3ab99f79375b4e999087270e
WanderVafla <procenkovladik1@gmail.com>
2026-06-10 08:46:38 +0200

    fix: enhanced logic of states

--- src/components/DeleteAllButton.tsx
+++ src/components/DeleteAllButton.tsx
@@ -1,43 +1,58 @@
-import { useRef, useState } from "react"
-import { useTodosStore } from "../store";
-import { ErrorMessage } from "../constants";
+import { useEffect, useRef, useState } from 'react';
+import { useTodosStore } from '../store';
+import { ErrorMessage } from '../constants';
+import type { Task } from '../types';
 
 export function DeleteAllDialog() {
-    const [opened, isOpened] = useState(false)
-    const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const [opened, isOpened] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
 
-    const tasks = useTodosStore(state => state.todos)
-    const deleteallTodos = useTodosStore((state) => state.deleteAllTodos)
-    const addError = useTodosStore(state => state.addError)
+  const tasks: Task[] | null = useTodosStore((state) => state.todos);
+  const deleteallTodos = useTodosStore((state) => state.deleteAllTodos);
+  const addError = useTodosStore((state) => state.addError);
 
-    if (dialogRef.current) {
-        if (opened) {
-            dialogRef.current.showModal()
-        } else {
-            dialogRef.current.close()
-        }
+  useEffect(() => {
+    if (dialogRef.current !== null) {
+      if (opened) {
+        dialogRef.current.showModal();
+      } else {
+        dialogRef.current.close();
+      }
     }
+  }, [opened, dialogRef]);
 
-    return (
-        <>
-            <button type="button" onClick={() => {
-                if (tasks.length > 0) {
-                    isOpened(true)
-                } else {
-                    addError(ErrorMessage.haveNotTasks)
-                }
-                }} >Remove All Task</button>
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          if (tasks !== null && tasks.length > 0) {
+            isOpened(true);
+          } else {
+            addError(ErrorMessage.haveNotTasks);
+          }
+        }}
+      >
+        Remove All Task
+      </button>
 
-            <dialog ref={dialogRef}>
-                <span>Want delete all your tasks?</span>
-                <div>
-                    <button type="button" onClick={() => {
-                        isOpened(false)
-                        deleteallTodos()
-                    }}>Yes</button>
-                    <button type="button" onClick={() => isOpened(false)} >No</button>
-                </div>
-            </dialog>
-        </>
-    )
+      <dialog ref={dialogRef} id="delete-all-modal">
+        <span>Want delete all your tasks?</span>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              isOpened(false);
+              deleteallTodos();
+            }}
+          >
+            Yes
+          </button>
+          <button type="button" onClick={() => isOpened(false)}>
+            No
+          </button>
+        </div>
+      </dialog>
+    </>
+  );
 }
